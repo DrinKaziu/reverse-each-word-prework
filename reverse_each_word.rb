ec765bad36adcb98e679c94d71a57f371255becc
Drin K <drinkaziu@gmail.com>
2018-04-17 21:55:50 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,9 +7,6 @@ def reverse_each_word(sentence)
   arr.join(" ")
 end
 
-# def reverse_each_word(sentence)
-#   new_sentence = sentence.split(" ")
-#   new_sentence.collect do |word|
-#     word.reverse.join(" ")
-#   end
-# end
+def reverse_each_word(sentence)
+
+end
