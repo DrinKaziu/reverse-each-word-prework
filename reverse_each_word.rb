bc9efe2f5f749912f113521fd8766a5a5bfc4faf
Drin K <drinkaziu@gmail.com>
2018-04-17 22:02:02 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(sentence)
-  new_sentence = sentence.split(" ")
+  new_sentence = sentence.split("")
   arr = []
   new_sentence.each do |word|
     arr << word.reverse
@@ -8,7 +8,7 @@ def reverse_each_word(sentence)
 end
 
 def reverse_each_word(sentence)
-  new_sentence = sentence.split(" ")
+  new_sentence = sentence.split("")
   new_sentence.collect do |word|
     word.reverse
   end
