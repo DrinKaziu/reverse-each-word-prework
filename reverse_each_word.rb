89de3fce34d916bdf2601ff3df1f151a6665f4d7
Drin K <drinkaziu@gmail.com>
2018-04-17 22:09:49 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word(sentence)
+  new_sentence = sentence.split(" ")
+  arr = []
+  new_sentence.each do |word|
+    arr << word.reverse
+  end
+  arr.join(" ")
+end
+
+
+def reverse_each_word(sentence)
+  sentence.split.collect {|word| word.reverse}.join(" ")
+end
