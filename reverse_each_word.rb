4389a202955d8fe2fba663d284b6d4820f5a2c2e
Drin K <drinkaziu@gmail.com>
2018-04-17 21:54:45 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,9 +7,9 @@ def reverse_each_word(sentence)
   arr.join(" ")
 end
 
-def reverse_each_word(sentence)
-  new_sentence = sentence.split(" ")
-  new_sentence.collect do |word|
-    word.reverse.join(" ")
-  end
-end
+# def reverse_each_word(sentence)
+#   new_sentence = sentence.split(" ")
+#   new_sentence.collect do |word|
+#     word.reverse.join(" ")
+#   end
+# end
