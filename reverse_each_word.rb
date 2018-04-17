a42f166c0a72dd8de37c2ec080a2e2e87538d719
Drin K <drinkaziu@gmail.com>
2018-04-17 21:50:57 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
   new_sentence = sentence.split
   arr = []
   new_sentence.each do |word|
-    arr.push(word.reverse)
+    arr << word.reverse
   end
   puts arr
 end
